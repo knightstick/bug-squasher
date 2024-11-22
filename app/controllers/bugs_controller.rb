8b28440b78aa28c29f4a0a0cb4f0b659f8f524ff
Chris Jewell <chrisjohnjewell@gmail.com>
2024-11-22 23:36:55 +0000

    Add bug submission functionality

--- app/controllers/bugs_controller.rb
+++ app/controllers/bugs_controller.rb
@@ -1,4 +1,23 @@
 class BugsController < ApplicationController
   def index
+    @bugs = Bug.all
+  end
+
+  def new
+    @bug = Bug.new
+  end
+
+  def create
+    @bug = Bug.new(bug_params)
+
+    @bug.save!
+
+    redirect_to bugs_path
+  end
+
+  private
+
+  def bug_params
+    params.expect(bug: [ :title, :description ])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  resources :bugs, only: %w[index new]
+  resources :bugs, only: %w[index new create]
 end
